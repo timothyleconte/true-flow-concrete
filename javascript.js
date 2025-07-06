4f8cb141d08ae5a888d9245df0c52b9eb9e6f599
timothyleconte <marktbooher@outlook.com>
2025-07-05 23:13:18 -0400

    Complete starter layout
    Will give suggestions and take input to further create the page

---
+++ javascript.js
@@ -0,0 +1,7 @@
+let joke = document.querySelector(".joke");
+
+joke.addEventListener("click", () =>{
+
+    alert("Josh, hope the unit isn't too bad tonight. Let me know what you think of our basic starter setup here!")
+
+})
