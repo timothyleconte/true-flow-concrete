82b676b7b58cb1b7bf34770c429b049068f19c32
timothyleconte <marktbooher@outlook.com>
2025-07-05 23:23:17 -0400

    Fix typo

--- javascript.js
+++ javascript.js
@@ -2,6 +2,6 @@ let joke = document.querySelector(".joke");
 
 joke.addEventListener("click", () =>{
 
-    alert("Josh, hope the unit isn't too bad tonight. Let me know what you think of our basic starter setup here!")
+    alert("Josh, hope the unit isn't too bad tonight. Let me know what you think of our basic starter setup here! Text me and I'll tell you my ideas haha")
 
 })
